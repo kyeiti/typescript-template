59c187c2cf4f051de2f7552e4f5f9a236dcd876c
kyeiti <kyeiti@vorsicht-bissig.de>
2023-10-19 20:44:38 +0200

    Improve infiltrate output

--- src/infiltrate.ts
+++ src/infiltrate.ts
@@ -1,4 +1,5 @@
 import {NS} from "@ns";
+import {printTable} from "/util/table";
 
 export async function main(ns: NS) {
   const locations = ns.infiltration.getPossibleLocations();
@@ -7,7 +8,14 @@ export async function main(ns: NS) {
     infiltrations.push(ns.infiltration.getInfiltration(location.name));
   }
   infiltrations.sort((a, b) => a.difficulty - b.difficulty);
-  for(const infiltration of infiltrations) {
-    ns.tprintf("%s - %s: %s", infiltration.location.city, infiltration.location.name, infiltration.difficulty)
-  }
+  printTable(ns.tprintf, [
+      [
+        {hTpl: '%19s', dTpl: '%19s', h: "City", k: {}, d: infiltrations.map((i) => i.location.city)},
+        {hTpl: '%25s', dTpl: '%25s', h: "Company", k: {}, d: infiltrations.map((i) => i.location.name)},
+        {hTpl: '%5s', dTpl: '%5.3f', h: "Dif", k: {}, d: infiltrations.map((i) => i.difficulty)},
+        {hTpl: '%7s', dTpl: '%7.2f', h: "SoA", k: {}, d: infiltrations.map((i) => i.reward.SoARep)},
+        {hTpl: '%9s', dTpl: '%9.2f', h: "Rep", k: {}, d: infiltrations.map((i) => i.reward.tradeRep)},
+      ]
+  ]
+  )
 }
