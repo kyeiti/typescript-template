7c505f6f531b5212bd173b23718be9fc8d620fc6
kyeiti <kyeiti@vorsicht-bissig.de>
2023-10-22 10:21:56 +0200

    Improve performace of prime-factor-solver

--- src/contracts/prime-factor.ts
+++ src/contracts/prime-factor.ts
@@ -1,5 +1,9 @@
 
 export function primeFactor(data: number) {
+    console.log(data, isPrime(data));
+    if(isPrime(data)) {
+        return data;
+    }
     const primeFactors = [];
     console.log(data)
     for(let i = 2, currentRemainingValue = data; i <= currentRemainingValue; i++) {
@@ -9,6 +13,11 @@ export function primeFactor(data: number) {
         if(currentRemainingValue % i === 0) {
             primeFactors.push(i);
             currentRemainingValue = currentRemainingValue / i;
+            if(isPrime(currentRemainingValue)) {
+                primeFactors.push(currentRemainingValue);
+                break
+            }
+            i--;
         }
     }
     console.log(primeFactors)
@@ -16,7 +25,9 @@ export function primeFactor(data: number) {
 }
 
 function isPrime(num: number) {
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if(num === 2) return true
+    if(num % 2 === 0) return false
+    for(let i = 3, s = Math.sqrt(num); i <= s; i =  i+2) {
         if(num % i === 0) return false;
     }
     return num > 1;
