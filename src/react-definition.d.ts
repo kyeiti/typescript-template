2b0e172918809c8cf95c796bdf5344ea739a47b4
kyeiti <kyeiti@vorsicht-bissig.de>
2023-11-12 12:45:08 +0100

    Fix: Add dependencies for backdoor and goto script

---
+++ src/react-definition.d.ts
@@ -0,0 +1,13 @@
+// import ReactNamespace from 'react/index';
+// import ReactDomNamespace from 'react-dom';
+
+// const React = window.React as typeof ReactNamespace;
+// const ReactDOM = window.ReactDOM as typeof ReactDomNamespace;
+
+declare const React: typeof import("react/index");
+declare const ReactDOM: typeof import("react-dom");
+
+// export default React;
+// export {
+//   ReactDOM
+// }
--- src/util/connect.ts
+++ src/util/connect.ts
@@ -1,9 +1,28 @@
 import {NS} from "@ns";
+import {Scanner, TreeNode} from "/cc/Scanner";
 
-export async function main(ns: NS) {
-
+export function connect(ns: NS, target: string) {
+    const scanner = new Scanner(ns);
+    const tree = scanner.tree;
+    let node = findInTree(tree, target);
+    const connections = []
+    while (node) {
+        connections.push(node.name);
+        node = node.parent ?? null
+    }
+    connections.reverse().forEach(n => ns.singularity.connect(n))
 }
 
-export function autocomplete(data: any, args: string[]) {
-    return [...data.servers];
+
+function findInTree(tree: TreeNode, key: string): TreeNode | null {
+    if (tree.name === key) {
+        return tree;
+    }
+    for (const child of tree.children) {
+        const found = findInTree(child, key);
+        if (found) {
+            return found;
+        }
+    }
+    return null;
 }
