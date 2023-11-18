c0eee1af11e47791d538a872095a7f76ee3884d3
kyeiti <kyeiti@vorsicht-bissig.de>
2023-11-18 12:03:57 +0100

    Fix: Stop trying to buy to many shares and imclude commissionFee in calculations

--- src/stock.ts
+++ src/stock.ts
@@ -1,38 +1,44 @@
 import {NS, TIX} from "@ns";
 import {formatMoney} from "/util/formatters";
 
+const commissionFee = 100e3;
 
 export async function main(ns: NS) {
     const stock = ns.stock;
     const symbols = stock.getSymbols();
     ns.disableLog("sleep")
     while (true) {
-        play(stock, symbols, ns.printf)
+        play(stock, ns, symbols, ns.printf)
         await ns.sleep(1000)
     }
 }
 
-function play(stock: TIX, symbols: readonly string[], printer: (format: string, ...args: any[]) => void) {
+function play(stock: TIX, ns: NS, symbols: readonly string[], printer: (format: string, ...args: any[]) => void) {
     for (const symbol of symbols) {
+        const player = ns.getPlayer();
         const maxShares = stock.getMaxShares(symbol)
         const owned = stock.getPosition(symbol);
         const longStock = owned[0];
-        const longPrice = owned[0] * owned[1]
+        const longPaid = owned[0] * owned[1] + commissionFee
         const shortStock = owned[2]
-        const shortPrice = owned[2] * owned[3]
+        const shortPaid = owned[2] * owned[3] + commissionFee
         const shortGain = stock.getSaleGain(symbol, maxShares, "Short")
         const longGain = stock.getSaleGain(symbol, longStock, "Long")
+        const longPrice = stock.getAskPrice(symbol)
+        const shortPrice = stock.getBidPrice(symbol)
+        const purchasableLongAmount = Math.min(Math.floor((player.money -commissionFee) / longPrice), maxShares);
+        const purchasableShortAmount = Math.min(Math.floor((player.money-commissionFee) / shortPrice), maxShares);
         if (stock.getForecast(symbol) > 0.5) {
-            if (longStock <= maxShares) {
-                const buy = stock.buyStock(symbol, maxShares - longStock)
+            if (purchasableLongAmount > 0) {
+                const buy = stock.buyStock(symbol, purchasableLongAmount - longStock)
                 if (buy > 0) {
-                    printer("Bought %s for %s", symbol, formatMoney(buy * (maxShares - longStock)))
+                    printer("Bought %s for %s", symbol, formatMoney(buy * (purchasableLongAmount - longStock) + commissionFee))
                 }
             }
             if (shortStock > 0) {
                 const sold = stock.sellShort(symbol, maxShares)
                 if (sold > 0) {
-                    const gain = shortGain - shortPrice;
+                    const gain = shortGain - shortPaid;
                     const formatted= formatMoney(gain);
                     if(gain > 0) {
                         printer("INFO Sold %s (short) for %s profit", symbol, formatted)
@@ -42,12 +48,16 @@ function play(stock: TIX, symbols: readonly string[], printer: (format: string, 
                 }
             }
         } else {
-            // const buy = stock.buyShort(symbol, maxShares)
-            // printer("Bought %s (short) for %d", symbol, buy)
+            // if (shortStock <= purchasableShortAmount) {
+            //     const buy = stock.buyShort(symbol, maxShares)
+            //     if (buy > 0) {
+            //         printer("Bought %s (short) for %d", symbol, formatMoney(buy * (purchasableShortAmount - shortStock)))
+            //     }
+            // }
             if (longStock > 0) {
                 const sold = stock.sellStock(symbol, longStock)
                 if (sold > 0) {
-                    const gain = longGain - longPrice;
+                    const gain = longGain - longPaid;
                     const formatted= formatMoney(gain);
                     if(gain > 0) {
                         printer("INFO Sold %s for %s profit", symbol, formatted)
